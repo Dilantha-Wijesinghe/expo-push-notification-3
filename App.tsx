229e9875aa6ca007edb39ab4c2b95ee09e459a70
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-05-06 11:09:23 +0530

    feat: First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { usePushNotifications } from "./usePushNotifications";
+import { useEffect } from "react";
 
 export default function App() {
+  const { expoPushToken, notification } = usePushNotifications();
+
+  const data = JSON.stringify(notification, undefined, 2);
+
+  useEffect(() => {
+    console.log(expoPushToken?.data);
+  }, [expoPushToken]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ marginVertical: 100 }}>
+      <View style={styles.container}>
+        <Text>Token: {expoPushToken?.data ?? ""}</Text>
+        <Text>{data}</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
